6a14acde744ec1f0731149b3cb8b86dea3a296b7
unknown <Adrian Stawarz>
2020-11-15 14:32:22 +0100

    router

---
+++ client/src/router.js
@@ -0,0 +1,9 @@
+export function router(event) {
+    if (typeof event.target.nextLink !== 'undefined')
+    {
+        return event.target.nextLink;
+    }
+    else{
+        return '/api/products'
+    }
+}
